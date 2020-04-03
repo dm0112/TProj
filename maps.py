704625f9ca03fa29f457adf4702c10f5fb86055d
lowlyN <37045405+lowlyN@users.noreply.github.com>
2020-04-03 13:49:06 +0300

    Update maps.py

--- maps.py
+++ maps.py
@@ -2,10 +2,15 @@ import googlemaps
 import urllib.request
 import json
 import apiKey
+country = "country:RO"
 rawGeo ='https://maps.googleapis.com/maps/api/geocode/json?'
 rawDir = "https://maps.googleapis.com/maps/api/directions/json?"
-def checkIfExists(name, country):
 
+def convToAscii(text):
+    return text.replace("ă","a").replace("î","i").replace("ț","t").replace("ș","s").replace("â","a").replace(' ','+')
+
+def checkIfExists(name, country):
+    name = convToAscii(name)
     onlyLoc = "address={}&key={}&components={}".format(name, apiKey.goagalKey, country)
     request = rawGeo + onlyLoc
     response = urllib.request.urlopen(request).read()
@@ -15,17 +20,19 @@ def checkIfExists(name, country):
     if(len(result['results'])):
         # print("coordinates:")
         # print(result['results'][0]['geometry']['location'])
-        return str((result['results'][0]['address_components'][0]['long_name']))
+        # print(result['results'][0]["formatted_address"])
+        print("found it")
+
+        return convToAscii(str((result['results'][0]['address_components'][0]['long_name'])))
+
     else:
         print("couldn't find it")
 
-
-loc = 'Iasi'.replace(' ','+')
-country = "country:RO"
 # print(loc)
 # print(checkIfExists(loc, country))
 
 # print(result['results'][0]['address_components'][0]['long_name'])
+
 
 def makeDir(orig, dest):
     onlyLoc = "origin={}&key={}&destination={}".format(orig, apiKey.goagalKey, dest)
@@ -36,4 +43,5 @@ def makeDir(orig, dest):
     print(result['routes'][0]['legs'][0]['distance'])
 
 
-makeDir(checkIfExists("Vaslui", country),checkIfExists("Cluj",country))
+# makeDir(checkIfExists("iasi", country),checkIfExists("Galati",country))
+checkIfExists("palatul culturii",country)
